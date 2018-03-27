7da2c7d826b145ca34eb8e3727bca453ca8670bf
fgr-17 <rouxfederico@gmail.com>
2018-03-27 20:03:23 -0300

    Ejercicio 2 con una sola fsm

--- misProyectos/ejer2-template/src/sAPI_program.c
+++ misProyectos/ejer2-template/src/sAPI_program.c
@@ -16,6 +16,16 @@ typedef enum {BUTTON_UP, BUTTON_FALLING, BUTTON_DOWN, BUTTON_RAISING} fsmAntirre
 
 typedef enum {TECLA_PRESIONADA, TECLA_SUELTA, TECLA_ERROR} tecla_t;
 
+typedef struct
+{
+	fsmAntirrebote_t estado;
+	delay_t delay;
+	tick_t tiempo_ventana;
+	gpioMap_t tecla_pin;
+	tecla_t t;
+}antirreb_t;
+
+
 /*==================[definiciones de datos internos]=========================*/
 
 #define DELAY_DURATION 1000
@@ -23,26 +33,34 @@ typedef enum {TECLA_PRESIONADA, TECLA_SUELTA, TECLA_ERROR} tecla_t;
 #define ANTIRREBOTE_DELAY	40
 
 fsmBlinky_t state = LED_ON;
-
-fsmAntirrebote_t fsmAntirrebote_estado = BUTTON_UP;
-fsmAntirrebote_t fsmAntirrebote2_estado = BUTTON_UP;
-
 delay_t blinkyDelay;
 
-delay_t antirreboteDelay;
+/**
+ * @
+ *
+ */
 
-gpioMap_t tecla_a1 = TEC1;
-gpioMap_t tecla_a2 = TEC2;
 
+antirreb_t antirreb_tecla1;
+antirreb_t antirreb_tecla2;
 
-tecla_t t1;
-tecla_t t2;
 /*==================[definiciones de datos externos]=========================*/
 
 /*==================[declaraciones de funciones internas]====================*/
 void fsmBlinky(void);
-void fsmAntirrebote (void);
-void Inicializar_fsmAntirrebote (void);
+
+void fsmAntirrebote1 (void);
+void Inicializar_fsmAntirrebote1 (void);
+
+void fsmAntirrebote2 (void);
+void Inicializar_fsmAntirrebote2 (void);
+
+void fsmAntirrebote (antirreb_t*antirreb);
+void Inicializar_fsmAntirrebote (antirreb_t*antirreb, gpioMap_t tecla_asigada);
+
+void ActualizarFrecuenciaParpadeo();
+void ActualizarLedActivo();
+
 /*==================[declaraciones de funciones externas]====================*/
 
 /*==================[funcion principal]======================================*/
@@ -50,29 +68,24 @@ void Inicializar_fsmAntirrebote (void);
 #define LEDS_L ( sizeof(leds) / sizeof(gpioMap_t) )
 #define PERIODOS_L ( sizeof(periodos) / sizeof( tick_t ) )
 
-#define PERIODO_50			50
-#define PERIODO_100			100
-
 #define TECLA_ABAJO			OFF
 #define TECLA_ARRIBA		ON
 
-tick_t periodos[] = {PERIODO_50, PERIODO_100};
-tick_t periodoActivo = PERIODO_50;
+tick_t periodos[] = {50, 100, 1000};
+tick_t periodoActivo = 50;
 int periodoIndice = 0;
 
-gpioMap_t leds[] = {LEDR, LEDG, LEDB};
+gpioMap_t leds[] = {LEDR, LEDG, LEDB, LED1, LED2, LED3};
 gpioMap_t ledActivo = LEDR;
 int ledIndice = 0;
 
 // FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
 int main( void ){
 
-
-
-
 	// ---------- CONFIGURACIONES ------------------------------
-	Inicializar_fsmAntirrebote();
-	Inicializar_fsmAntirrebote2();
+	Inicializar_fsmAntirrebote(&antirreb_tecla1, TEC1);
+	Inicializar_fsmAntirrebote(&antirreb_tecla2, TEC2);
+
 	// Inicializar y configurar la plataforma
 	boardConfig();
 
@@ -80,39 +93,10 @@ int main( void ){
 	while( TRUE )
 	{
 		fsmBlinky();
-		fsmAntirrebote();
-		fsmAntirrebote2();
-
-		if (t1 == TECLA_PRESIONADA){
-			periodoIndice++;
-			if (periodoIndice >= PERIODOS_L)
-				periodoIndice = 0;
-
-			periodoActivo = periodos[periodoIndice];
-
-
-			t1 = TECLA_SUELTA;
-		}
-
-		if (t2 == TECLA_PRESIONADA){
-			ledIndice++;
-			if (ledIndice >= LEDS_L)
-				ledIndice = 0;
-
-			gpioWrite(ledActivo, 0);	// apago el led anterior
-			ledActivo = leds[ledIndice];
-
-
-			t2 = TECLA_SUELTA;
-		}
-
-
-		// Escribir el valor leido en el LED correspondiente.
-		// gpioWrite( LEDR, tec1Value );
-
-
-
-
+		fsmAntirrebote(&antirreb_tecla1);
+		fsmAntirrebote(&antirreb_tecla2);
+		ActualizarFrecuenciaParpadeo();
+		ActualizarLedActivo();
 	}
 
 	// NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta
@@ -125,6 +109,48 @@ int main( void ){
 
 /*==================[definiciones de funciones internas]=====================*/
 
+/**
+ * @fn void ActualizarFrecuenciaParpadeo()
+ *
+ *
+ */
+
+void ActualizarFrecuenciaParpadeo()
+{
+	if (antirreb_tecla1.t == TECLA_PRESIONADA){
+		periodoIndice++;
+		if (periodoIndice >= PERIODOS_L)
+			periodoIndice = 0;
+
+		periodoActivo = periodos[periodoIndice];
+
+
+		antirreb_tecla1.t = TECLA_SUELTA;
+	}
+	return;
+}
+
+/**
+ * @fn void ActualizarFrecuenciaParpadeo()
+ *
+ *
+ */
+
+void ActualizarLedActivo()
+{
+	if (antirreb_tecla2.t == TECLA_PRESIONADA){
+		ledIndice++;
+		if (ledIndice >= LEDS_L)
+			ledIndice = 0;
+
+		gpioWrite(ledActivo, 0);	// apago el led anterior
+		ledActivo = leds[ledIndice];
+
+
+		antirreb_tecla2.t = TECLA_SUELTA;
+	}
+	return;
+}
 
 /**
  * @fn fsmBlinky
@@ -174,16 +200,16 @@ void fsmBlinky(void){
 }
 
 /**
- * @fn void Inicializar_fsmAntirrebote (void)
- *
+ * @fn void Inicializar_fsmAntirrebote2 (void)
  *
  */
 
+void Inicializar_fsmAntirrebote (antirreb_t*antirreb, gpioMap_t tecla_asigada ){
 
-void Inicializar_fsmAntirrebote (void){
-
-	fsmAntirrebote_estado = BUTTON_UP;
-	t1 = TECLA_SUELTA;
+	antirreb->estado = BUTTON_UP;
+	antirreb->t = TECLA_SUELTA;
+	antirreb->tiempo_ventana = ANTIRREBOTE_DELAY;
+	antirreb->tecla_pin = tecla_asigada;
 }
 
 /**
@@ -194,58 +220,58 @@ void Inicializar_fsmAntirrebote (void){
  *
  */
 
-void fsmAntirrebote (void){
+void fsmAntirrebote (antirreb_t*antirreb){
 
 	bool_t tecValue = TECLA_ARRIBA;
 
 
-	switch(fsmAntirrebote_estado)
+	switch(antirreb->estado)
 	{
 
 	case BUTTON_UP:
-		tecValue = gpioRead( tecla_a1 );
+		tecValue = gpioRead( antirreb->tecla_pin );
 		if(tecValue == TECLA_ARRIBA){
-			fsmAntirrebote_estado = BUTTON_UP;
+			antirreb->estado = BUTTON_UP;
 		}
 		else if (tecValue == TECLA_ABAJO)
 		{
-			fsmAntirrebote_estado = BUTTON_FALLING;
-			delayConfig(&antirreboteDelay, ANTIRREBOTE_DELAY);
+			antirreb->estado = BUTTON_FALLING;
+			delayConfig(&antirreb->delay, antirreb->tiempo_ventana);
 			/* para que el delay arranque hay que leerlo*/
-			delayRead(&antirreboteDelay);
+			delayRead(&antirreb->delay);
 		}
 
 		break;
 	case BUTTON_FALLING:
 
 		/* Si el tiempo del delay expiró, paso a leer el estado del pin */
-		if (delayRead(&antirreboteDelay)){
-			tecValue = gpioRead( tecla_a1 );
+		if (delayRead(&antirreb->delay)){
+			tecValue = gpioRead( antirreb->tecla_pin );
 			if(tecValue == TECLA_ARRIBA){
-				fsmAntirrebote_estado = BUTTON_UP;
+				antirreb->estado = BUTTON_UP;
 			}
 			else if(tecValue == TECLA_ABAJO){
-				fsmAntirrebote_estado = BUTTON_DOWN;
-				t1 = TECLA_PRESIONADA;
+				antirreb->estado = BUTTON_DOWN;
+				antirreb->t = TECLA_PRESIONADA;
 			}
 
 		}
 		else{
-			fsmAntirrebote_estado = BUTTON_FALLING;
+			antirreb->estado = BUTTON_FALLING;
 		}
 		break;
 	case  BUTTON_DOWN:
 
-		tecValue = gpioRead( tecla_a1 );
+		tecValue = gpioRead( antirreb->tecla_pin );
 		if(tecValue == TECLA_ABAJO){
-			fsmAntirrebote_estado = BUTTON_DOWN;
+			antirreb->estado = BUTTON_DOWN;
 		}
 		else if (tecValue == TECLA_ARRIBA)
 		{
-			fsmAntirrebote_estado = BUTTON_RAISING;
-			delayConfig(&antirreboteDelay, ANTIRREBOTE_DELAY);
+			antirreb->estado = BUTTON_RAISING;
+			delayConfig(&antirreb->delay, antirreb->tiempo_ventana);
 			/* para que el delay arranque hay que leerlo*/
-			delayRead(&antirreboteDelay);
+			delayRead(&antirreb->delay);
 		}
 		break;
 
@@ -253,125 +279,28 @@ void fsmAntirrebote (void){
 
 
 		/* Si el tiempo del delay expiró, paso a leer el estado del pin */
-		if (delayRead(&antirreboteDelay)){
-			tecValue = gpioRead( tecla_a1 );
+		if (delayRead(&antirreb->delay)){
+			tecValue = gpioRead( antirreb->tecla_pin );
 			if(tecValue == TECLA_ABAJO){
-				fsmAntirrebote_estado = BUTTON_DOWN;
+				antirreb->estado = BUTTON_DOWN;
 			}
 			else if(tecValue == TECLA_ARRIBA){
-				fsmAntirrebote_estado = BUTTON_UP;
+				antirreb->estado = BUTTON_UP;
 			}
 
 		}
 		else{
-			fsmAntirrebote_estado = BUTTON_RAISING;
+			antirreb->estado = BUTTON_RAISING;
 		}
 		break;
 
 	default:
-		Inicializar_fsmAntirrebote();
+		Inicializar_fsmAntirrebote(antirreb, antirreb->tecla_pin);
 
 	}
 
 	return;
 }
-
-void Inicializar_fsmAntirrebote2 (void){
-
-	fsmAntirrebote_estado = BUTTON_UP;
-	t2 = TECLA_SUELTA;
-}
-
-/**
- *
- * @fn void fsmAntirrebote (void)
- *
- *
- *
- */
-
-void fsmAntirrebote2 (void){
-
-	bool_t tecValue = TECLA_ARRIBA;
-
-
-	switch(fsmAntirrebote2_estado)
-	{
-
-	case BUTTON_UP:
-		tecValue = gpioRead( tecla_a2 );
-		if(tecValue == TECLA_ARRIBA){
-			fsmAntirrebote2_estado = BUTTON_UP;
-		}
-		else if (tecValue == TECLA_ABAJO)
-		{
-			fsmAntirrebote2_estado = BUTTON_FALLING;
-			delayConfig(&antirreboteDelay, ANTIRREBOTE_DELAY);
-			/* para que el delay arranque hay que leerlo*/
-			delayRead(&antirreboteDelay);
-		}
-
-		break;
-	case BUTTON_FALLING:
-
-		/* Si el tiempo del delay expiró, paso a leer el estado del pin */
-		if (delayRead(&antirreboteDelay)){
-			tecValue = gpioRead( tecla_a2 );
-			if(tecValue == TECLA_ARRIBA){
-				fsmAntirrebote2_estado = BUTTON_UP;
-			}
-			else if(tecValue == TECLA_ABAJO){
-				fsmAntirrebote2_estado = BUTTON_DOWN;
-				t2 = TECLA_PRESIONADA;
-			}
-
-		}
-		else{
-			fsmAntirrebote2_estado = BUTTON_FALLING;
-		}
-		break;
-	case  BUTTON_DOWN:
-
-		tecValue = gpioRead( tecla_a2 );
-		if(tecValue == TECLA_ABAJO){
-			fsmAntirrebote2_estado = BUTTON_DOWN;
-		}
-		else if (tecValue == TECLA_ARRIBA)
-		{
-			fsmAntirrebote2_estado = BUTTON_RAISING;
-			delayConfig(&antirreboteDelay, ANTIRREBOTE_DELAY);
-			/* para que el delay arranque hay que leerlo*/
-			delayRead(&antirreboteDelay);
-		}
-		break;
-
-	case BUTTON_RAISING:
-
-
-		/* Si el tiempo del delay expiró, paso a leer el estado del pin */
-		if (delayRead(&antirreboteDelay)){
-			tecValue = gpioRead( tecla_a2 );
-			if(tecValue == TECLA_ABAJO){
-				fsmAntirrebote2_estado = BUTTON_DOWN;
-			}
-			else if(tecValue == TECLA_ARRIBA){
-				fsmAntirrebote2_estado = BUTTON_UP;
-			}
-
-		}
-		else{
-			fsmAntirrebote2_estado = BUTTON_RAISING;
-		}
-		break;
-
-	default:
-		Inicializar_fsmAntirrebote2();
-
-	}
-
-	return;
-}
-
 
 /*==================[definiciones de funciones externas]=====================*/
 
